675b29ad1aee04569dc876b85a82cbfd2fb3dd16
robertome <roberto.martin.escribano@alumnos.upm.es>
2018-10-12 12:05:10 +0200

    Incluido package 'entities' con las entidades del proyecto

---
+++ src/main/java/es/upm/miw/apaw/api/entities/Alumno.java
@@ -0,0 +1,94 @@
+package es.upm.miw.apaw.api.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Alumno {
+
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private Profesor profesor;
+    private Set<Practica> practicas = new HashSet<>();
+
+    public Alumno() {
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public void update(Practica practica) {
+        practicas.add(practica);
+    }
+
+    public Set<Practica> getPracticas() {
+        return practicas;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    static class Builder {
+        private Alumno alumno = new Alumno();
+
+        public Builder id(String id) {
+            assert id != null && !id.isEmpty();
+
+            alumno.setId(id);
+            return this;
+        }
+
+        public Builder nombre(String nombre) {
+            assert nombre != null && !nombre.isEmpty();
+
+            alumno.setNombre(nombre);
+            return this;
+        }
+
+        public Builder apellidos(String apellidos) {
+            assert apellidos != null && !apellidos.isEmpty();
+
+            alumno.setApellidos(apellidos);
+            return this;
+        }
+
+        public Builder profesor(Profesor profesor) {
+            alumno.setProfesor(profesor);
+            return this;
+        }
+
+        public Alumno build() {
+            return alumno;
+        }
+    }
+}
---
+++ src/main/java/es/upm/miw/apaw/api/entities/Profesor.java
@@ -0,0 +1,15 @@
+package es.upm.miw.apaw.api.entities;
+
+import java.util.Set;
+
+public class Profesor {
+
+    private String id;
+    private String nombre;
+    private String apellidos;
+
+    public Profesor() {
+    }
+
+
+}
